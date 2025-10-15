1ba8db5c29377595f17a01ece6839f3baf16bfc2
George Pricop <96602369+Gzeu@users.noreply.github.com>
2025-10-16 00:54:17 +0300

    feat: Add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: [
+    'api/**/*.js',
+    '!api/tests/**',
+    '!**/node_modules/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
+  verbose: true,
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov', 'html']
+};
